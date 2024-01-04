c1483cfaeb0131be5038735791bde35ea5cb4aad
Dylan James <alazyartist@gmail.com>
2024-01-03 23:58:15 -0700

    all of the things

--- blink.py
+++ blink.py
@@ -2,12 +2,12 @@ from machine import Pin
 from time import sleep
 
 led = Pin("LED", Pin.OUT)
-led2 = Pin("GP0", Pin.OUT)
+# led2 = Pin("GP0", Pin.OUT)
 
 while True:
     led.on()
-    led2.on()
+    # led2.on()
     sleep(0.25)
     led.off()
-    led2.off()
+    # led2.off()
     sleep(0.25)
